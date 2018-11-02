5ddc5499514bc20f50e49bafa80aa7ebf77ca9b6
Sajjon <alex.cyon@gmail.com>
2018-11-02 15:16:29 +0100

    Initial Commit

---
+++ SwiftCompilerFails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCompilerFails
+//
+//  Created by Alexander Cyon on 2018-11-02.
+//  Copyright © 2018 Alex Cyon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
